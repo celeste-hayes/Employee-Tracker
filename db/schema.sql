fb59e15adb47c3c50f38ef78eceeb75c178a4540
Celeste Hayes <hayes.celeste93@gmail.com>
2024-12-10 21:16:22 -0500

    feat: add schema.sql code

---
+++ db/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE DATABASE employee_tracker;
+
+\c employee_tracker;
+
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    FOREIGN KEY (department_id) 
+    REFERENCES departments(department_id) 
+    ON DELETE CASCADE
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) 
+    REFERENCES roles(role_id) 
+    ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) 
+    REFERENCES employees(id) 
+    ON DELETE CASCADE
+);
