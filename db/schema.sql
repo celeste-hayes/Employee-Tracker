a92f6a2addece6f4ff7cf74db785b3eba7e303a3
Celeste Hayes <hayes.celeste93@gmail.com>
2024-12-15 20:36:55 -0500

    fix: update naming convention mismatches between schema.sql and server.js file

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,28 @@
+-- Drop and recreate the database
 DROP DATABASE IF EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 
+-- Connect to the created database
 \c employee_tracker;
 
+-- Create the department table
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
 );
 
+-- Create the role table
 CREATE TABLE role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INTEGER NOT NULL,
     FOREIGN KEY (department_id) 
-    REFERENCES departments(department_id) 
+    REFERENCES department(id) 
     ON DELETE CASCADE
 );
 
+-- Create the employees table
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -25,7 +30,7 @@ CREATE TABLE employees (
     role_id INTEGER NOT NULL,
     manager_id INTEGER,
     FOREIGN KEY (role_id) 
-    REFERENCES roles(role_id) 
+    REFERENCES role(id) 
     ON DELETE CASCADE,
     FOREIGN KEY (manager_id) 
     REFERENCES employees(id) 
